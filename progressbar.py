6cd715a766a879fe104c91551f6ba5b936d2daab
Yu Kiyokawa <y-kiyokawa@cq.jp.nec.com>
2019-04-21 23:52:22 +0900

    [fix]refs# progressbar

---
+++ progressbar.py
@@ -0,0 +1,53 @@
+import sys, time
+from IPython.display import display
+from IPython.display import clear_output
+class progbar:
+    def __init__(self, period=100, bars=32):
+        self._period  = period
+        self.bars     = bars
+        self.active   = True
+        self.start    = time.time()
+
+    def dispose(self):
+        if self.active:
+            self.active = False
+            self.update(self._period)
+            sys.stdout.write("\n")
+
+    def __del__(self):
+        self.dispose()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.dispose()
+
+    def period(self):
+        return self._period
+
+    def update(self, tick):
+        clear_output()
+        rate = tick / self._period
+
+        # progress rate
+        str = "{0:7d}% ".format(int(rate*100))
+
+        # progress bar
+        bar_prog = int(rate * self.bars)
+        str += "|"
+        str += "#" * (            bar_prog)
+        str += "-" * (self.bars - bar_prog)
+        str += "|"
+
+        # calc end
+        elapsed = time.time() - self.start
+        predict = (elapsed * (1 - rate) / rate) if not rate == 0.0 else 0
+        s = int(predict)
+        m, s = divmod(s, 60)
+        h, m = divmod(m, 60)
+        d, h = divmod(h, 24)
+        str += "{0:3d}:{1:02d}:{2:02d}\r".format(h,m,s)
+#         sys.stdout.write("\r {}".format(str))
+#         sys.stdout.flush()
+        display("{0}".format(str))
