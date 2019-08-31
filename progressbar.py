c9cf42569117de2cd1bac5a67f54061de055e96d
Yu Kiyokawa <y-kiyokawa@cq.jp.nec.com>
2019-08-31 10:42:06 +0900

    [update] Changed display clear function to work on command line

--- progressbar.py
+++ progressbar.py
@@ -2,17 +2,23 @@ import sys, time
 from IPython.display import display
 from IPython.display import clear_output
 class progbar:
-    def __init__(self, period=100, bars=32,clear_display=True):
+    def __init__(self, period=100, bars=32, display='command',clear_display=True):
+        assert display in ['notebook','command'], 'display type: [notebook, command]'
+
         self._period  = period
         self.bars     = bars
         self.active   = True
         self.start    = time.time()
+        self.display  = display
         self.clear_disp = clear_display
+        self.elapsed = 0
+        self.initial_update = True
 
     def dispose(self):
         if self.active:
             self.active = False
-            self.update(self._period)
+            timeinfo = self.calc_time(self.elapsed)
+            self.update(self._period,info='Total time:{0}'.format(timeinfo))
             sys.stdout.write("\n")
 
     def __del__(self):
@@ -28,8 +34,6 @@ class progbar:
         return self._period
 
     def update(self, tick,info=''):
-        if self.clear_disp:
-            clear_output()
         rate = tick / self._period
 
         # progress rate
@@ -43,11 +47,27 @@ class progbar:
         str += "|"
 
         # calc end
-        elapsed = time.time() - self.start
-        predict = (elapsed * (1 - rate) / rate) if not rate == 0.0 else 0
-        s = int(predict)
+        self.elapsed = time.time() - self.start
+        predict = (self.elapsed * (1 - rate) / rate) if not rate == 0.0 else 0
+        timeinfo = self.calc_time(predict)
+        str += timeinfo
+
+        if self.display == 'notebook':
+            if self.clear_disp:
+                clear_output()
+            display("{0} {1}".format(str,info))
+        if self.display == 'command':
+            if self.clear_disp and not(self.initial_update):
+                sys.stdout.write("\033[1A\033[2K\033[G")
+            sys.stdout.write("{0} {1}\n".format(str,info))
+            sys.stdout.flush()
+
+        self.initial_update = False
+    
+    def calc_time(self,t):
+        s = int(t)
         m, s = divmod(s, 60)
         h, m = divmod(m, 60)
         d, h = divmod(h, 24)
-        str += " {day}day{hour:3d}:{minute:02d}:{sec:02d}".format(day=d,hour=h,minute=m,sec=s)
-        display("{0} {1}".format(str,info))
+        timeinfo = " {day}day{hour:3d}:{minute:02d}:{sec:02d}".format(day=d,hour=h,minute=m,sec=s)
+        return timeinfo
