07818d2914b8d40c3df8cb3763c57cd334586fed
JohnYKiyo <dummn.marionette.7surspecies@gmail.com>
2020-04-06 01:22:17 +0900

    [update] update usage and sample script

--- test.py
+++ test.py
@@ -3,23 +3,23 @@ import time
 
 def main():
     print('test')
-    bar = progressbar.progbar(10,display='command',clear_display=True)
+    bar = progressbar.progbar(10,clear_display=True)
     for i in range(10):
         time.sleep(0.1)
         bar.update(i)
     del bar
 
     print('test2')
-    bar = progressbar.progbar(10,display='command',clear_display=False)
+    bar = progressbar.progbar(10,clear_display=False)
     for i in range(10):
         time.sleep(0.1)
         bar.update(i)
     del bar
 
     print('test3')
-    bar = progressbar.progbar(10,display='command',clear_display=True)
+    bar = progressbar.progbar(10,clear_display=True)
     for j in range(10):
-        prog = progressbar.progbar(10,display='command',clear_display=True)
+        prog = progressbar.progbar(10,clear_display=True)
         for i in range(10):
             time.sleep(0.1)
             prog.update(i)
